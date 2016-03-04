9c5894520f4207092ab48ba9be4fb1290a2591ff
Stephen Seo <seo.disparate@gmail.com>
2016-03-04 21:39:25 +0900

    Changed Bitset to use Combined lists

--- src/EC/Bitset.hpp
+++ src/EC/Bitset.hpp
@@ -4,6 +4,7 @@
 
 #include <bitset>
 #include "Meta/TypeList.hpp"
+#include "Meta/Combine.hpp"
 
 namespace EC
 {
@@ -11,16 +12,18 @@ namespace EC
     struct Bitset :
         public std::bitset<ComponentsList::size + TagsList::size>
     {
+        using Combined = EC::Meta::Combine<ComponentsList, TagsList>;
+
         template <typename Component>
         constexpr auto getComponentBit()
         {
-            return (*this)[EC::Meta::IndexOf<Component, ComponentsList>::value];
+            return (*this)[EC::Meta::IndexOf<Component, Combined>::value];
         }
 
         template <typename Tag>
         constexpr auto getTagBit()
         {
-            return (*this)[ComponentsList::size + EC::Meta::IndexOf<Tag, TagsList>::value];
+            return (*this)[EC::Meta::IndexOf<Tag, Combined>::value];
         }
     };
 }
