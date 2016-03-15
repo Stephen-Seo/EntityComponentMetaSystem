a00883037347eef60cc0cb9606da0afef36cb204
Stephen Seo <seo.disparate@gmail.com>
2016-03-15 19:29:13 +0900

    Added License and legal related requirements
    All source files in src/EC now has some text describing
    attribution notice to the original work this work derives from.

--- src/EC/Meta/Morph.hpp
+++ src/EC/Meta/Morph.hpp
@@ -1,4 +1,8 @@
 
+// This work derives from Vittorio Romeo's code used for cppcon 2015 licensed under the Academic Free License.
+// His code is available here: https://github.com/SuperV1234/cppcon2015
+
+
 #ifndef EC_META_MORPH_HPP
 #define EC_META_MORPH_HPP
 
